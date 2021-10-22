5c09f775a3c9d21a97d0436163a63a662e73a298
yogurtsyum <28582329+yogurtsyum@users.noreply.github.com>
2021-10-22 18:06:14 -0500

    add delete route and make keys go in body

--- index.js
+++ index.js
@@ -43,8 +43,10 @@ async function createid(size) {
   return result;
 }
 
+app.use(express.json());
+
 app.post('/', async (req, res) => {
-  if(req.query.key !== key) return res.send('NO_PERMISION');
+  if(req.body.key !== key) return res.send('NO_PERMISION');
   if(!req.files.file) return res.send('NO_FILE');
   let id = await createid(7);
   let extension = req.files.file.name.split('.')[1];
@@ -60,6 +62,17 @@ app.get('/:filename', async (req, res) => {
   if(findfile == -1) findfile = filelist.findIndex((file) => file.fname === filename);
   if(findfile == -1) return res.send('FILE_NOT_FOUND');
   res.sendFile(__dirname + '/files/' + filelist[findfile].filelocation);
+});
+
+app.post('/delete/:filename', async (req, res) => {
+  if(req.body.key !== key) return res.send('NO_PERMISION');
+  let filename = req.params.filename;
+  if(!filename) return res.send('FILE_NOT_FOUND');
+  let findfile = filelist.findIndex((file) => file.name === filename);
+  if(findfile == -1) findfile = filelist.findIndex((file) => file.fname === filename);
+  if(findfile == -1) return res.send('FILE_NOT_FOUND');
+  fs.unlink(__dirname + '/files/' + filelist[findfile].filelocation);
+  res.send('Done');
 });
 
 const listener = app.listen(3000, () => {
